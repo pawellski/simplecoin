c8c52c38dfe40a13f31824f6e3f43693c5c1de89
pawellski <skibaapawell@gmail.com>
2022-11-01 22:24:15 +0100

    Add controller (host.py) and move functionalities to KeyManager

--- app/model/key_manager.py
+++ app/model/key_manager.py
@@ -1,10 +1,12 @@
-from ecdsa import SigningKey, VerifyingKey, NIST384p
+from ecdsa import SigningKey, VerifyingKey, NIST384p, BadSignatureError
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
+from requests import post
 from os import environ
 import base64, json
+from flask import jsonify
 
 import logging
 
@@ -78,29 +80,145 @@ class KeyManager:
             'pub_key': self.__pub_key.to_pem().decode('utf-8')
         })
 
-    def get_private_key(self):
-        return self.__priv_key
-    
-    def get_public_key(self):
-        return self.__pub_key
+    def __get_pub_key_for_ip(self, ip_without_port):
+        for entry in self.__pub_key_list['entries']:
+            if ip_without_port in entry['ip']:
+                return entry['pub_key']
+        return None
 
-    def get_curr_ip(self):
-        return self.__ip
+    def __format_ip(self, ip, endpoint):
+        return f"{ip}/{endpoint}"
 
-    def update_pub_key_list(self, new_pub_key_list):
+    def __request_pub_key_list_update(self, ip):
+        if ip != self.__ip:
+            self.__log.info(f"Requesting pub key update for ip {ip}")
+            try:
+                res = post(self.__format_ip(ip, 'update'), json = self.__pub_key_list)
+                return True if res.ok else False
+
+            except Exception as e:
+                self.__log.error(f"Pub key request for ip {ip} failed, reason: {e}")
+                return False
+
+        self.__log.info(f"Ip matches host's ip, skipping")
+        return True
+
+    def __update_pub_key_list(self, new_pub_key_list):
         for entry in new_pub_key_list['entries']:
             if entry in self.__pub_key_list['entries']:
                 continue
             self.__pub_key_list['entries'].append(entry)
 
-    def override_pub_key_list(self, new_pub_key_list):
+    def __override_pub_key_list(self, new_pub_key_list):
         self.__pub_key_list = new_pub_key_list
-    
+
+    def __request_pub_key_list_updates(self):
+        self.__log.info(f"Starting process for updating connected nodes' public key lists, node count: {len(self.__pub_key_list) - 1}") # minus current node's ip
+        for el in self.__pub_key_list['entries']:
+            res = self.__request_pub_key_list_update(el['ip'])
+            if not res:
+                self.__log.info(f"Public key update process failed")
+                return False, el['ip']
+        return True, None
+
+    def __verify_sender(self, ip, signed_message, message):
+        pub_key = self.__get_pub_key_for_ip(ip)
+        if pub_key is None:
+            return False
+        try:
+            vk = VerifyingKey.from_pem(pub_key)
+            decrypted_message = base64.b64decode(signed_message.encode('utf-8'))
+            return vk.verify(decrypted_message, message.encode('utf-8'))
+
+        except (BadSignatureError, TypeError) as e:
+            self.__log.error(f"Veryfing massage failed, reason: {e}")
+            return False
+
+    def __sign_message(self, message):
+        return self.__priv_key.sign(message.encode('utf-8'))
+
     def get_pub_key_list(self):
         return self.__pub_key_list
 
-    def get_pub_key_for_ip(self, ip_without_port):
-        for entry in self.__pub_key_list['entries']:
-            if ip_without_port in entry['ip']:
-                return entry['pub_key']
-        return None
+    def connect(self, request_data):
+        ip = request_data['ip']
+        self.__log.info(f"Received request to join new network, target node: {ip}")
+        body = self.get_pub_key_list()
+
+        try:
+            self.__log.info(f"Sending request to join network to target node: {ip}")
+            res = post(self.__format_ip(ip, 'join'), json = body)
+            if res.ok:
+                self.__log.info(f"Sucessfully joined new network, received {res.content}")
+                return jsonify(self.get_pub_key_list())
+        except Exception as e:
+            error_message = f"Error connecting to network, target node: {ip}, exception: {e}"
+            self.__log.error(error_message)
+            e.message = error_message
+            raise
+
+    def join(self, request_data):
+        self.__log.info(f"Received request to join nodes to current network, received {request_data}")
+        try:
+            self.__update_pub_key_list(request_data)
+            self.__log.info(f'New entires added to current public key list')
+        except Exception as e:
+            error_message = f"Error on saving new entries in public key list, error: {e}"
+            self.__log.error(error_message)
+            e.message = error_message
+            raise
+        result, ip = self.__request_pub_key_list_updates()
+        if not result:
+            raise Exception(f"Error updating list for address {ip}")
+        return "Successfully added new node(s) to network"
+
+    def send_message(self, request_data):
+        ip = request_data['ip']
+        message = request_data['message']
+        signed_message = self.__sign_message(message)
+
+        body = {
+            "ip": self.__ip,
+            "signed_message": base64.b64encode(signed_message).decode('utf-8'),
+            "plaintext": message
+        }
+
+        try:
+            res = post(self.__format_ip(ip, '/verify-message-from-node'), json = body)
+            if res.ok:
+                self.__log.info(f"Got: {res.content}")
+                return res.content
+            else:
+                raise Exception("Error sending message")
+        except Exception as e:
+            e.message = f"Error encountered during call to receive message, error: {e}"
+            raise
+
+
+    def receive_message(self, request_data):
+        ip = request_data['ip']
+        signed_message = request_data['signed_message']
+        plaintext = request_data['plaintext']
+
+        try:
+            body = {}
+            if self.__verify_sender(ip, signed_message, plaintext):
+                body['confirmation'] = 'True'
+                body['message_from_veryfier'] = 'Message signed correctly'
+            else:
+                body['confirmation'] = 'False'
+                body['message_from_veryfier'] = 'Message signed incorrectly'
+            body['sended_message'] = plaintext
+            return jsonify(body)
+        except Exception as e:
+            e.message = f"Error encountered during veryfing message, error: {e}"
+            raise
+
+    def update(self, request_data):
+        self.__log.info(f"Received request to update public key list, new list: {request_data}")
+        try:
+            self.__override_pub_key_list(request_data)
+            return "List updated succesfully"
+        except Exception as e:
+            e.message = f"Error encountered during call to update list, error: {e}"
+            raise
