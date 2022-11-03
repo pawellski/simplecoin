dec0dc198b10b7b6183f3094c863cce6422012ba
pawellski <skibaapawell@gmail.com>
2022-11-03 22:19:00 +0100

    Add block class

---
+++ app/model/block.py
@@ -0,0 +1,83 @@
+from hashlib import sha256
+
+class Block:
+    def __init__(self, previous_block_hash, nonce, transactions, previous_block):
+        # first part of block which contains important information 
+        self.__header = self.Header(previous_block_hash, nonce)
+        # second part of block which contains transactions
+        self.__data = self.Data(transactions)
+        # reference to previous block
+        self.__previous_block = previous_block
+
+    def set_previous_block(self, block):
+        self.__previous_block = block
+
+    def get_previous_block(self):
+        return self.__previous_block
+
+    def get_header(self):
+        return self.__header
+
+    def get_data(self):
+        return self.__data
+
+    """
+    Return block as a dictionary with two
+    important keys - header and data
+    """
+    def to_dict(self):
+        block = {}
+        block['header'] = self.__header.to_dict()
+        block['data'] = self.__data.get_transactions()
+        return block
+
+    """
+    Calculate and return sha256
+    bases on cast dictionary to str
+    """
+    def get_hash(self):
+        data = str(self.to_dict())
+        return sha256(data.encode('utf-8')).hexdigest()
+
+
+    class Header:
+        def __init__(self, previous_block_hash, nonce):
+            self.__previous_block_hash = previous_block_hash
+            self.__nonce = nonce
+
+        def get_previous_block_hash(self):
+            return self.__previous_block_hash
+
+        def get_nonce(self):
+            return self.__nonce
+
+        def set_previous_block_hash(self, previous_block_hash):
+            self.__previous_block_hash = previous_block_hash
+
+        def set_nonce(self, nonce):
+            self.__nonce = nonce
+
+        """
+        Return header as a dictionary
+        """
+        def to_dict(self):
+            header = {}
+            header['previous_block_hash'] = self.__previous_block_hash
+            header['nonce'] = self.__nonce
+            return header
+
+    class Data:
+        def __init__(self, transactions):
+            self.__transactions = transactions
+
+        def set_transactions(self, transactions):
+            self.__transactions = transactions
+
+        def get_transactions(self):
+            return self.__transactions
+
+        def add_transaction(self, transaction):
+            self.__transactions.append(transaction)
+
+        def clear_transactions(self):
+            self.__transactions = []
