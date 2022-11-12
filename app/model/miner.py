d10bdb410b6772be3bcebda78031e18b8ac9adda
Control11 <Control11@gmail.com>
2022-11-12 16:36:48 +0100

    delete implementation of update_transaction_pool

--- app/model/miner.py
+++ app/model/miner.py
@@ -3,7 +3,4 @@ class Miner:
         self.__log = log
 
     def update_transaction_pool(self, request_data):
-        message = request_data["message"]
-        signed_message = request_data["signed_message"]
-        self.__log.info(f"!!!!!!!!!  TRANSACTION POOL UPDATED   !!!!!!!!!   plaintext = {message} , signed = {signed_message}")
         return "Success"
